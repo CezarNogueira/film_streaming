d2ff92fde90f356acf70d20dad28ae5309c6dcf0
Cezar Nogueira <cezartv1221@gmail.com>
2025-04-12 09:28:57 -0300

    Primeiro Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao Screen Match");
+
+        System.out.println("FIlme: Top Gun: Maverick");
+
+        int realease_year = 2022;
+        System.out.println("Ano de Lançamento: " + realease_year);
+
+        boolean include_plan = true;
+        double film_grade = 8.1;
+        String synopsis;
+        synopsis = "Filme de aventura";
+
+        System.out.println("Nota: " + film_grade);
+        System.out.println("Sinopse: " + synopsis);
+    }
+}
