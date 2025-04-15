1e4f52bf63e5f33f5081bfe3bd38aa9bcfcdfcd8
Cezar Nogueira <cezartv1221@gmail.com>
2025-04-15 20:01:47 -0300

    Feat: Criar for para notas

--- src/Loop.java
+++ src/Loop.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Loop {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        double meanRating = 0;
+        double rating = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Diga sua avaliação para o filme:");
+            rating = read.nextDouble();
+            meanRating += rating;
+        }
+
+        System.out.println("Média de avaliações: " + meanRating / 3);
+
+    }
 }
