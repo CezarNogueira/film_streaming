2a988815147f9f90aed978e58399d967baf77bb6
Cezar Nogueira <cezartv1221@gmail.com>
2025-04-16 20:50:02 -0300

    Feat: Definir metodos como Privados e Adicionar Settelers e Getters

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import movie.model.Movie;
 public class Main {
     public static void main(String[] args) {
         Movie myMovie = new Movie();
-        myMovie.name = "Harry Potter";
-        myMovie.releaseYear = 2014;
-        myMovie.includePlan = true;
+        myMovie.setName("Harry Potter");
+        myMovie.setReleaseYear(2009);
+        myMovie.setIncludePlan(true);
 
         myMovie.infoMovie();
         myMovie.rateMovie(3.5);
         myMovie.rateMovie(4.5);
-        System.out.println(myMovie.meanRating);
+        System.out.println(myMovie.getMeanRating());
         System.out.println(myMovie.getAllRating());
     }
 }
--- src/movie/model/Movie.java
+++ src/movie/model/Movie.java
@@ -1,12 +1,12 @@
 package movie.model;
 
 public class Movie {
-    public String name;
-    public int releaseYear;
-    public boolean includePlan;
+    private String name;
+    private int releaseYear;
+    private boolean includePlan;
     private int allRating;
-    public double meanRating;
-    public int duration;
+    private double meanRating;
+    private int duration;
 
     public void infoMovie() {
         System.out.println("Nome do Filme: " + name);
@@ -20,5 +20,29 @@ public class Movie {
 
     public int getAllRating() {
         return allRating;
+    }
+
+    public double getMeanRating() {
+        return meanRating;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIncludePlan(boolean includePlan) {
+        this.includePlan = includePlan;
+    }
+
+    public void setMeanRating(double meanRating) {
+        this.meanRating = meanRating;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
     }
 }
