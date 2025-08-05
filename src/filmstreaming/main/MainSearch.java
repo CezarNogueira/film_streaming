35c55fd9bd8105a04eb2e46a61fecdb06bfff644
Cezar Nogueira <cezartv1221@gmail.com>
2025-08-05 11:32:25 -0300

    Feat: Refinar a Busca com API

--- src/filmstreaming/main/MainSearch.java
+++ src/filmstreaming/main/MainSearch.java
@@ -5,13 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Digite um Filme para Buscar: ");
+        var search = read.nextLine();
+        String adress = "http://www.omdbapi.com/?t=" + search + "&apikey=a3d93235";
+
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=a3d93235"))
+                .uri(URI.create(adress))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
